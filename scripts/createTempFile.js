13682c506caee5825dbde2c150d8d14f7f0f5d53
Romuald Brillout <git@brillout.com>
2021-07-15 16:37:51 +0200

    wip

--- scripts/createTempFile.js
+++ scripts/createTempFile.js
@@ -10,7 +10,20 @@ mkdirp(helpersPath).then(() => {
     `module.exports = (msg)=>{return \`hello \${String(msg)} !!! it works.\`}`,
     (err) => {
       if (err) throw err;
-      console.log("Build time file created successfully!");
+      console.log("Build time serverless function created successfully!");
+    }
+  );
+});
+
+const distPath = path.join(__dirname, "..", "public");
+
+mkdirp(distPath).then(() => {
+  fs.writeFile(
+    path.join(distPath, "index.html"),
+    `Some static HTML`,
+    (err) => {
+      if (err) throw err;
+      console.log("Build time static files created successfully!");
     }
   );
 });
